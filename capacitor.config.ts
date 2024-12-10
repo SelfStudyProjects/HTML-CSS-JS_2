3cf53e617a82d631c839413acd9f83f4dade2241
najasinis <tttjjjyyy1234@daum.net>
2024-12-10 11:31:12 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eoullim',
+  webDir: 'dist'
+};
+
+export default config;
